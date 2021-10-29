3aecf792ea7436870d4b69b2a4d2e494bf700c94
Debora Souza <deboral.p@hotmail.com>
2021-10-29 11:29:26 -0300

    Adicionar atributos na classe VendedorDTO

---
+++ src/main/java/br/com/zup/Ecommerce/EcomerceDTO/VendedorDTO.java
@@ -0,0 +1,8 @@
+package br.com.zup.Ecommerce.EcomerceDTO;
+
+public class VendedorDTO {
+    private String email;
+    private String nome;
+    private String telefone;
+    //composição de lista de produtos
+}
