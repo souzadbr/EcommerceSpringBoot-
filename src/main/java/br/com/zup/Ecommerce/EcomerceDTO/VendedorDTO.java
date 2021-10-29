ac9b83e30f89e1f8cc71ae1c92057b4fcad42b00
Debora Souza <deboral.p@hotmail.com>
2021-10-29 11:33:30 -0300

    Modelar classe VendedorDTO

--- src/main/java/br/com/zup/Ecommerce/EcomerceDTO/VendedorDTO.java
+++ src/main/java/br/com/zup/Ecommerce/EcomerceDTO/VendedorDTO.java
@@ -1,9 +1,46 @@
 package br.com.zup.Ecommerce.EcomerceDTO;
 
+import java.util.List;
+
 public class VendedorDTO {
     private String email;
     private String nome;
     private String telefone;
-    //composição de lista de produtos
+    private ProdutoDTO produtoDTOS;
 
+    public VendedorDTO() {
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public ProdutoDTO getProdutoDTOS() {
+        return produtoDTOS;
+    }
+
+    public void setProdutoDTOS(ProdutoDTO produtoDTOS) {
+        this.produtoDTOS = produtoDTOS;
+    }
 }
